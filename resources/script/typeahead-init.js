67b8c51c80e105118d0bfdedd236c6ce908537d6
Tobi Krebs <tobias.krebs@betterform.de>
2013-10-29 14:26:32 +0100

    [services] - extract earliest/latest date from viaf

--- resources/script/typeahead-init.js
+++ resources/script/typeahead-init.js
@@ -22,11 +22,11 @@ function initAutocompletes() {
         jObject.val(xfValue.val());
         
         var name = jObject.attr("name");            
-        var query = jObject.attr("query");
+        var queryType = jObject.attr("queryType");
         var host = jObject.attr("host");
         
         
-        console.log("Name: " , name , " Query: " , query, ' HOST:', host);
+        console.log("Name: " , name , " queryType: " , queryType, ' HOST:', host);
        /*
             <result>
                 <name>Not found</name>
@@ -41,15 +41,18 @@ function initAutocompletes() {
             </result>
         */ 
         jObject.typeahead({
-            name: query,
+            name: queryType,
             dataType: "json",
             minLength: 3,
             limit: 20,
             template: '<div class="th_wrapper">'
-                        + '<span class="th_resources img_{{resource}}"></span>'
+                        + '<div class="th_resources">'
+                        +   '<span class="img_{{resource}}"></span>'
+                        + '</div>'
                         + '<div class="th_text_wrapper">'
-                        +   '<div class="th_value" title="{{hint}}">{{value}}</div>'
+                        +   '<div class="th_value" title="{{type}}">{{value}}</div>'
                         +   '{{#bio}}<div class="th_bio">{{/bio}}{{bio}}{{#bio}}</div>{{/bio}}'
+                        +   '{{#hint}}<div class="th_hint" title="{{hint}}"/>{{/hint}}'
                         + '</div>'
                         + '<div class="th_sources">'
                         +   '{{#sources}}<span class="img_{{.}}"></span>{{/sources}}'
@@ -57,7 +60,7 @@ function initAutocompletes() {
                     + '</div>',
             engine: Hogan,
             remote: {
-                url: host + '/exist/apps/cluster-services/modules/services/search/search.xql?' + query + '=%QUERY',
+                url: host + '/exist/apps/cluster-services/modules/services/search/search.xql?' + queryType + '=%QUERY',
                 filter: function(parsedData){
                     var dataset = [];
                     if( Object.prototype.toString.call( parsedData.result ) === '[object Array]' ) {
@@ -75,13 +78,15 @@ function initAutocompletes() {
                             
                             
                             
-                            console.log("Sources: ", sourcesArray);
+                            //console.log("Sources: ", sourcesArray);
                             
                             dataset.push({
                                 name: parsedData.result[i].name,
                                 value: parsedData.result[i].value,
                                 internalID: parsedData.result[i].internalID,
                                 bio: bio,
+                                earliestDate: parsedData.result[i].earliestDate,
+                                latestDate: parsedData.result[i].latestDate,
                                 uuid: parsedData.result[i].uuid,
                                 resource: parsedData.result[i].resource,
                                 type: parsedData.result[i].type,
@@ -91,13 +96,15 @@ function initAutocompletes() {
                         }
                     } else {
                         var bio = null;
-                        if(parsedData.result[i].bio !== undefined && parsedData.result[i].bio !== '') { bio = parsedData.result.bio };
+                        if(parsedData.result.bio !== undefined && parsedData.result.bio !== '') { bio = parsedData.result.bio };
                             
                         dataset.push({
                             name: parsedData.result.name,
                             value: parsedData.result.value,
                             internalID: parsedData.result.internalID,
                             bio: bio,
+                            earliestDate: parsedData.result.earliestDate,
+                            latestDate: parsedData.result.latestDate,
                             uuid: parsedData.result.uuid,
                             resource: parsedData.result.resource,
                             type: parsedData.result.type,
@@ -109,6 +116,7 @@ function initAutocompletes() {
                 }
             }
         }).on('typeahead:selected', function (e, datum) {
+            console.log("typeahead:selected : ", datum)
             var target = jQuery(e.target);
             var xfValue = $('#' + jObject.attr('callbackSet') + ' .xfRepeatIndex .' + jObject.attr('name') + ' .xfValue');
             xfValue.val(datum.name)
@@ -118,6 +126,38 @@ function initAutocompletes() {
             console.log("ID: " , id)
             fluxProcessor.sendValue("" + id, datum.name);
             
+            if(target.attr('queryType') === 'names') {
+                console.log("Names query.")
+                if(datum.bio != null) {
+                   console.log("Name BIO:", datum.bio);
+                   console.log("Name earliestDate:", datum.earliestDate);
+                   console.log("Name latestDate:", datum.latestDate);
+                   
+                   if(datum.earliestDate != null) {
+                       var earliestDateW = $('#' + jObject.attr('callbackSet') + ' .xfRepeatIndex input[placeholder=EarliestDate]');
+                       console.log("Name earliestDate:", earliestDateW);
+                       earliestDateW.val(datum.earliestDate);
+                       var id = earliestDateW.attr('id')
+                       id = id.substring(0, id.indexOf('-value'));
+                       console.log("ID: " , id);
+                       fluxProcessor.sendValue("" + id, datum.earliestDate);
+                   }
+                   
+                   if(datum.latestDate != null) {
+                       var latestDateW = $('#' + jObject.attr('callbackSet') + ' .xfRepeatIndex input[placeholder=LatestDate]');
+                       console.log("Name earliestDate:", latestDateW);
+                       latestDateW.val(datum.latestDate);
+                       var id = latestDateW.attr('id')
+                       id = id.substring(0, id.indexOf('-value'));
+                       console.log("ID: " , id);
+                       fluxProcessor.sendValue("" + id, datum.latestDate);
+                   }
+                }
+            } else if(target.attr(queryType) === 'subjects') {
+                console.log("Subjects query.")
+            } else {
+                console.log("Unknown query.")
+            }
         });
         
         autocompletes.push(jObject);
